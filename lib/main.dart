1431f151b0f1fe48dcd4f738c6bf2083dd1af638
BasirHami <132570859+BasirHami@users.noreply.github.com>
2023-06-14 13:52:16 +0430

    first commmit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Dice'),
-        ),
-        body: MyApp(),
-      ),
-    ),
-  );
+ runApp(MaterialApp(
+   home:Scaffold(appBar: AppBar(title: Text('Dice GAme'),
+    backgroundColor: colors .red,  ),
+     body: Dicegame(),
+     ) ,
+ ));
+
+}
+class Dicegame extends StatefulWidget {
+
+
+  @override
+  State<Dicegame> createState() => _DicegameState();
 }
 
-class MyApp extends StatelessWidget {
+class _DicegameState extends State<Dicegame> {
+
+  int leftdice = 1;
+  int rightdice = 2;
+  rolldice(){
+    setState(() {
+      leftdice = Random().nextInt(6)+1;
+      rightdice = Random().nextInt(6)+1;
+    });
+  }
+
+  rolldices(){
+    leftdice = Random().nextInt(6)+1;
+    rightdice = Random().nextInt(6)+1;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return row(
+      childern:[
+        Expanded(
+          child: FlatButton(
+            onpressed:() = print('some thing for now '),
+              child: Image.asset(Image path)),
+        ),
+
+    Expanded(
+    child: FlatButton(
+    onpressed:() = print('some thing for now '),
+    child: Image.asset(Image path)),
+      ],
+    );
   }
 }
+
